7f347dfd3b57617f206a40c5f27a0d8798a1b24b
Keren Dong <keren.dong+cx@kungfu.link>
2026-09-15 08:38:12 +0800

    fix(pipeline): bind source check to its actual recorder job
    Signed-off-by: Keren Dong <keren.dong+cx@kungfu.link>

--- actions/workflow/pipeline/record-build/index.js
+++ actions/workflow/pipeline/record-build/index.js
@@ -1,3 +1,3 @@
 import { runAction } from "../../../../packages/core/runtime/action-host.js";
-import { recordPipelineBuildAction } from "../../../../packages/core/workflow/pipeline/actions.js";
-await runAction(recordPipelineBuildAction);
+import { recordPipelineCheckAction } from "../../../../packages/core/workflow/pipeline/record-check.js";
+await runAction(recordPipelineCheckAction);
